432f923b774f5875cd639a45edd39072657824ca
Victor Sui <victorsui11@gmail.com>
2017-11-11 15:46:12 -0600

    Add simple TCP server
    For debugging purposes

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"net"
 
 	"github.com/anteater2/chord-node/utils"
 
@@ -28,5 +29,21 @@ func main() {
 		fmt.Printf("Creating network with %d addresses\n", config.MaxKey())
 	} else {
 		fmt.Printf("Connecting to network at %s\n", config.Introducer())
+	}
+
+	l, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer l.Close()
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		log.Printf("Hi there\n")
+		conn.Close()
 	}
 }
