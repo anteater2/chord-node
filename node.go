44916ea4607c6c4c93ccffe542123b04f34b5df1
Rohit Singh <singhrohit2@hotmail.com>
2017-11-12 16:09:47 -0600

    Folded startup code into node.go
    This file currently does not work!

--- node.go
+++ node.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"math/rand"
+	"net"
 
+	"./key"
 	"github.com/anteater2/bitmesh/rpc"
 	"github.com/anteater2/chord-node/config"
-	"github.com/anteater2/chord-node/key"
+	"github.com/anteater2/chord-node/utils"
 )
 
 var Address string
@@ -18,43 +22,40 @@ var RPCCallee *rpc.Callee
 
 var RPCFindSuccessor rpc.RemoteFunc
 var RPCNotify rpc.RemoteFunc
+var RPCGetPredecessor rpc.RemoteFunc
 
 // RemoteNode holds information for connecting to a remote node
 type RemoteNode struct {
 	Address string
 	Key     key.Key
-	IsNil   bool
 }
 
 // ClosestPrecedingNode finds the closest preceding node to the key in this node's finger table.
 // This doesn't need any RPC.
 func ClosestPrecedingNode(key key.Key) RemoteNode {
 	for i := config.NumFingers() - 1; i >= 0; i-- {
-		if Fingers[i].IsNil {
-			panic("You attempted to find closestPrecedingNode without an initialized finger table!")
+		if Fingers[i] == nil {
+			panic("You attempted to find ClosestPrecedingNode without an initialized finger table!")
 		}
-		if key.Between(Key, key) {
+		if key.BetweenExclusive(Key, key) {
 			return *Fingers[i]
 		}
 	}
-	return RemoteNode{Address: Address, Key: Key, IsNil: false}
+	return RemoteNode{Address: Address, Key: Key}
 }
 
 // FindSuccessor finds the successor node to the key.  This may require RPC calls.
 func FindSuccessor(key key.Key) RemoteNode {
-	if key.Between(Key, Successor.Key) {
+	if key.BetweenEndInclusive(Key, Successor.Key) {
 		// key is between this node and its successor
-		return RemoteNode{Address: Address, Key: Key, IsNil: false}
+		return RemoteNode{Address: Address, Key: Key}
 	}
-
 	target := ClosestPrecedingNode(key)
 	// Now, we have to do an RPC on target to find the successor.
 	interf, err := RPCFindSuccessor(target.Address+":2000", uint64(key))
-
 	if err != nil {
-		panic("RPC failed")
+		panic("RPC FindSuccessor Failed!")
 	}
-
 	rv := interf.(RemoteNode)
 	return rv
 }
@@ -67,41 +68,97 @@ func Notify(node RemoteNode) int {
 	return 0 // Necessary to interface with RPCCaller
 }
 
+func GetPredecessor() RemoteNode {
+	return *Predecessor
+}
+
+//Stabilize the Successor and Predecessor fields of this node.
+//This is a goroutine and never terminates.
+func Stabilize() {
+	Successor.Address
+}
+
+// CreateLocalNode creates a local node on its own ring.  It can be inserted into another ring later.
 func CreateLocalNode() {
 	// Set the variables of this node.
 	Key = key.Key(rand.Uint64() % config.MaxKey()) // Use a random key for now because addresses are all the same rn
-
 	RPCCaller, err := rpc.NewCaller(config.CallerPort())
 	if err != nil {
 		panic("RPCCaller failed to initialize")
 	}
-
 	RPCCallee, _ := rpc.NewCallee(config.CalleePort())
 	if err != nil {
 		panic("RPCCallee failed to initialize")
 	}
 
-	Address = "127.0.0.1" // TODO: Make the address resolve to the real address of the node.
+	Address = GetOutboundIP()
 	Predecessor = nil
 	Successor = &RemoteNode{
-		Address: "127.0.0.1", // TODO: Make the address resolve to the real address of the node.
+		Address: Address,
 		Key:     Key,
-		IsNil:   false,
+	}
+	// Initialize the finger table for the solo ring configuration
+	for i := uint64(0); i < config.NumFingers(); i++ {
+		Fingers[i] = Successor
 	}
 
 	// Define all of the RPC functions.  For more info, look at Yuchen's caller.go and example_test.go
 	RPCFindSuccessor = RPCCaller.Declare(key.NewKey(1), RemoteNode{}, 10)
 	RPCNotify = RPCCaller.Declare(RemoteNode{}, 0, 10)
-
+	RPCGetPredecessor = RPCCaller.Declare(interface{}, RemoteNode{}, 10)
 	// RPCIsAlive = RPCCaller.Declare(nil{}, nil{}, 0) // Don't define this just yet - how does the RPC system react if a node fails to respond?
+
 	// Hook the RPCCallee into this node's functions
-	RPCCallee.Implement(FindSuccessor) // What happens if two methods have the same arg type signature?
+	RPCCallee.Implement(FindSuccessor)
 	RPCCallee.Implement(Notify)
 }
 
-// func main() {
-// 	CreateLocalNode()
-// 	go RPCCallee.Start()
-// 	go RPCCaller.Start()
-// 	// add the period functions here
-// }
+// Join a ring given a node IP address.
+func Join(ring string) {
+	ringCallee := ring + string(config.CalleePort())
+	ringSuccessorInterf, err := RPCFindSuccessor(ringCallee, Key)
+	if err != nil {
+		panic("RPCFindSuccessor failed!")
+	}
+	ringSuccessor := ringSuccessorInterf.(RemoteNode)
+	Successor = &ringSuccessor
+}
+
+// GetOutboundIP gets preferred outbound IP of this machine using a filthy hack
+// The connection should not actually require the Google DNS service (the 8.8.8.8),
+// but by creating it we can see what our preferred IP is.
+func GetOutboundIP() string {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+	return localAddr.IP.String()
+}
+func main() {
+	err := config.Init()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	addrs, err := utils.LocalAddrs()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, addr := range addrs {
+		fmt.Printf("%s\n", addr.String())
+	}
+
+	fmt.Printf("Creating local node on its own ring...\n")
+	CreateLocalNode()
+	go RPCCallee.Start()
+	go RPCCaller.Start()
+	if !config.Creator() {
+		fmt.Printf("Connecting node to network at %s\n", config.Introducer())
+		Join(config.Introducer())
+	}
+
+}
